db3cb68120cbfb6731f2ace8f3826f4f35de1f24
Yuetingzhang <jefferyz@futuredial.com>
2021-06-17 11:18:47 -0700

    first commit

---
+++ restart_devices.bat
@@ -0,0 +1,53 @@
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008020-000178141EF9002E
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008020-0012795C018A002E
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008020-00141CC63C08002E
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008020-0014558C02B9002E
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008020-00191D211E60003A
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008020-001C14492191002E
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008060-0011216A3CA0802E
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 00008060-001C75A03600802E
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 0520871832576054f8b01d8a44bba38928ac8f8e
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 1c54319dc1bdb017141bfcf70445764666595478
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 209d76673d86880183777d2825f95bb75c3fe528
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 29db231e134269ba6d37acfc672156f23b197482
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 39c51ea698bcafdb18806945bf7ea9d855246b58
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 3bc6b26dd2c205bc825dd9472aac92e119c47622
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 3d5dcef2fb3368e6b9e65b6b7159c88bc48cd80c
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 4521a06effc138f90f221f95cb1e4be4745b39eb
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 4e06c27909ed64ea4ae8951f84b53f9a24ac4b1a
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 4edcb65501d7052deb3394c36f0ff3af9b0a47da
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 5179595f1aac60d57977db3956fb380609402fbc
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 66a69e5cb43893f45a1ac75a59bf5c50a81512f4
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 6c4d4c47d2d204eb3e891fff6b721b5acdb20956
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 70e03c3a6ca10f1bbfa98839b163cd5acb95032c
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 71df45cdf10cceb4f5e565cfeb57fd4b66d9a0de
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 73f8fec5e5c09bc17ef7e3d267110d732a045364
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 882f2d8933dca5ecdb3fbc7fdaaa62cf921a93e7
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 8c5f7708dd504765a723e2097c34ed85f4875f00
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 9250c74aa504b6bbdd5fb0666455b853d29d6416
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 932d0b8fc43f6925a8d551867ea71be2e56dc5e3
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 952a98d53216668b90a3e944c7f2b7f5603df354
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u 974e41fc034ca760785b032dff8cfbbbb8fa0d05
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u b2160b9d369668012712d3760a0c1fb50782d7a7
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u d26b16266a86231ee80717a6a92d291cfadcfeb5
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u d299680dad908d15bbf25f8099e53495dbba7dd7
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u d8043d0b7023e484f3bbab2a940b9ad0e48b8fb0
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u dfe3240bbf5855c077eeb639cd35405af45bcf78
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u eb83816a1b6cf27273899ef1425e9137cabf1818
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u ed0a0ae5975aac1b3cd96fa8ba022a85c72ce7cf
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u edd8a3344614bdef1c6ae5d615260deaf4c3c197
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u fea355b8a82f2d7f879b90996e6006bdc99a0e4f
+echo timeout /t 60
+start C:\ProgramData\Futuredial\CMC\phonedll\PST_APE_UNIVERSAL_USB_FD\resource\iDeviceUtilCore.exe --restart 1 -u feb5ac3e31834c7e98f4cd103950aa21c3198e9b
